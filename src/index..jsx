788c4642eb2a602ee41a542f93985bc91d7cc7fa
Luis Sanchez <luis.sanchez.dev@gmail.com>
2023-07-31 23:02:58 -0500

    features: add jest

--- src/index..jsx
+++ src/index..jsx
@@ -1,10 +1,60 @@
 import React from 'react'
+import { createStore } from 'redux'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const noteReducer = (state = [], action) => {
+  if (action.type === 'NEW_NOTE') {
+    state.concat(action.data)
+    return state
+  }
+
+  return state
+}
+
+const store = createStore(noteReducer)
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2
+  }
+})
+
+const showStore = store.getState()
+console.log({ showStore })
+
+const App = () => {
+  return(
+    <div>
+      <ul>
+        {store.getState().map(note=>
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        )}
+        </ul>
+    </div>
+  )
+}
+
+const reducerApp = () => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+}
+
+reducerApp()
+store.subscribe(reducerApp)
